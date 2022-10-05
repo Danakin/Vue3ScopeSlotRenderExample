ee1d6633dde41f2f84a8f6a10e0fc1509934dace
Danny Festor <danny.festor@gmail.com>
2022-10-05 16:08:35 +0900

    Base app...

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import { ref } from 'vue';
+import TagsInput from './components/TagsInput.vue';
+
+  const tags = ref([
+    'asdf','wsdf','esdf'
+  ]);
+</script>
+
+<template>
+  <div>
+    <TagsInput v-model:tags="tags" />
+  </div>
+</template>
---
+++ src/components/TagsInput.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps(["tags"]);
+const emit = defineEmits(["update:tags"]);
+
+const newTag = ref("");
+
+const handleTagBackspace = (e) => {
+  if (newTag.value.length === 0) {
+    emit("update:tags", props.tags.slice(0, -1));
+  }
+};
+
+const addTag = (e) => {
+    if (newTag.value.length === 0 || props.tags.includes(newTag.value)){
+        return;
+    }
+    emit('update:tags', [...props.tags, newTag.value]);
+    newTag.value = '';
+};
+
+const removeTag = (tag) => {
+    emit('update:tags', props.tags.filter(t => t !== tag));
+};
+</script>
+
+<template>
+  <div class="tags-input">
+    <span v-for="tag in tags" class="tags-input-tag">
+      <span>{{ tag }}</span>
+      <button type="button" @click="removeTag(tag)" class="tags-input-remove">
+        &times;
+      </button>
+    </span>
+    <input
+      type="text"
+      class="tags-input-text"
+      placeholder="Add tag..."
+      @keydown.backspace="handleTagBackspace"
+      @keydown.enter.prevent="addTag"
+      v-model="newTag"
+    />
+  </div>
+</template>
